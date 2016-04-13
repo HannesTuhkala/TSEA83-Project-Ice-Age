3ddcb19aad2a546ed0bd910c58143cb7f087840c
HannesTuhkala <hantu447@student.liu.se>
2016-04-13 11:26:37 +0200

    Updated PM

--- proj/cpu.vhd
+++ proj/cpu.vhd
@@ -16,23 +16,23 @@ architecture behavioral of cpu is
 	----------------------------------------------------
 	-- Declaration of a doubleported block-RAM
 	-- with 512 addresses of 32 bits width.
-	type ram_t is array(0 to 511) of
+	type pm_t is array(0 to 511) of
 		std_logic_vector(31 downto 0);
 		
 	-- Reset all bits on all adresses
-	signal ram : ram_t := (others => (others => '0'));
+	signal pm : pm_t := (others => (others => '0'));
 	
 	-- port 1
-	adress1 : in std_logic_vector(8 downto 0);
-	rw1 : in std_logic; -- Read/Write flag for port 1
-	ce1 : in std_logic; -- Count enable flag for port 1
-	data1 : inout std_logic_vector(31 downto 0); -- Our instruction, which is 32 bits long.
+	signal adress1 : std_logic_vector(8 downto 0);
+	signal rw1 : std_logic; -- Read/Write flag for port 1
+	signal ce1 : std_logic; -- Count enable flag for port 1
+	signal data1 : std_logic_vector(31 downto 0); -- Our instruction, which is 32 bits long.
 	
 	-- port 2
-	adress2 : in std_logic_vector(8 downto 0);
-	rw2 : in std_logic; -- Read/Write flag for port 2
-	ce2 : in std_logic; -- Count enable flag for port 2
-	data2 : out std_logic_vector(31 downto 0)); -- Our instruction, which is 32 bits long.
+	signal adress2 : std_logic_vector(8 downto 0);
+	signal rw2 : std_logic; -- Read/Write flag for port 2
+	signal ce2 : std_logic; -- Count enable flag for port 2
+	signal data2 : std_logic_vector(31 downto 0)); -- Our instruction, which is 32 bits long.
 
 	--------------------------------------------------
 	--------------END OF PROGRAM MEMORY---------------
@@ -44,20 +44,8 @@ begin
 	PROCESS(clk)
 	BEGIN
 		if (rising_edge(clk)) then
-			-- synchronized read/write port 1
-			if (ce1 = '0') then
-				if (rw1 = '0') then
-					ram(adress1) <= data1;
-				else 
-					data1 <= (adress1);
-				end if;
-			end if;
-			
-			-- synchronized read port 2
-			if (ce2 = '0') then
-				if (rw2 = '0') then
-					data2 <= ram(adress2); -- Ev lägg till skrivning 
-				end if;
+			if (re1 = '0') then
+				data1 <= (adress1);
 			end if;
 		end if;
 	END PROCESS;
