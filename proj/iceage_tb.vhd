7891737205d94ca380b658b34396e586066efd1f
David Schützer <David@c-213-235.eduroam.liu.se>
2016-04-27 08:39:10 +0200

    fix missing tbfile

---
+++ proj/iceage_tb.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity Decoder is
+end Decoder;
+
+architecture Behavioral of Decoder_tb is
+
+  component lab
+      Port (
+  	    clk : in STD_LOGIC;
+	    joystick: in std_logic_vector(7 downto 0);
+           );
+  end component;
+
+  -- Testsignaler
+  signal clk : STD_LOGIC := '0';
+  signal joystick : std_logic_vector(7 downto 0) := others '0';
+begin
+
+  uut: lab PORT MAP(
+    clk => clk,
+    joystick => joystick);
+  -- Klocksignal 100MHz
+  clk <= not clk after 5 ns;
+
+
+end;
