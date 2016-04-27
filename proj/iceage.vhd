a93d8a403897fd9323829b7ef440895b82d35658
David Schützer <David@c-213-235.eduroam.liu.se>
2016-04-27 09:05:53 +0200

    added testbench for cpu

--- proj/iceage.vhd
+++ proj/iceage.vhd
@@ -1,6 +1,6 @@
 entity iceage
 	port (
-		c1 : in ???;
+		clk : in std_logic;;
 		c2 : out ???;
 		c3 : out ???);
 end iceage;
@@ -9,7 +9,7 @@ architecture behavioral of iceage is
 
 	component cpu
 		port (
-			a1 : in ???;
+			clk : in std_logic;;
 			a2 : out ???);
 	end component cpu;
 	
@@ -23,7 +23,7 @@ architecture behavioral of iceage is
 	signal x : ???;
 	
 begin
-	U0 : cpu port map(a1=>c1, a2=>x);
+	U0 : cpu port map(clk=>clk, a2=>x);
 	U1 : graphic port map(b1=>x, b2=>c2, b3=>c3);
 
 end behavioral;
