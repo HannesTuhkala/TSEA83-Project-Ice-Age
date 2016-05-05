103cee45efa0cfba195d94d2cac936e29765315c
Hannes Tuhkala <hannesmt@hotmail.com>
2016-05-05 14:21:44 +0200

    Added assembler and example code

--- assembler.py
+++ assembler.py
@@ -1,4 +1,145 @@
-#TSEA83 Assembler
-def convert(instruction): #Add r1, r2  använd .vsplit(' ')
-	instruction=instruction.vsplit(' ')
+import sys
+
+OP_CODES = {
+	"NOP" : 0,
+	"MOVE" : 1,
+	"ADD" : 3,
+	"SUB" : 4,
+	"MULT" : 5,
+	"SHIFT" : 6,
+	"COL" : 7,
+	"CMP" : 8,
+	"SETF" : 9,
+	"BRF" : 10,
+	"BRA" : 11,
+	"HALT" : 15,
+}
+
+MODES = {
+	"IMED" : "00",
+	"DIR" : "01",
+	"LEFT" : "00",
+	"RIGHT" : "01",
+	"Z0" : "00",
+	"Z1" : "01",
+	"N0" : "10",
+	"N1" : "11",
+	"Z" : "00",
+	"N" : "10",
+}
+
+COL = {
+	"UP" : "00",
+	"DOWN" : "01",
+	"LEFT" : "10",
+	"RIGHT" : "11",
+}
+
+def assemble(filename):
+	currentLine = 0
+	lines = {}
+	outputcode = {}
+	labels = {}
+
+	with open(filename) as f:
+		lines = f.readlines()
 	
+	for line in lines:
+		# Remove the newline and tab character
+		line = line.replace("\n", "")
+		line = line.replace("\t", "")
+		outputcode[currentLine] = parseLine(line, currentLine, labels)
+		currentLine += 1
+	
+	currentLine = 0
+	for line in outputcode:
+		if (outputcode[line] == ""):
+			continue;
+		print(str(currentLine) + ": " + outputcode[line] + " : " + "{0:0>4X}".format(int(outputcode[line], 2)))
+		currentLine += 1
+	
+def parseLine(line, currentLine, labels):
+	outputLine = ""
+	
+	line = line.upper()
+	words = line.split(" ")
+	
+	for key, word in enumerate(words):
+		if isHex(word):
+			words[key] = int(word[1:], 16)
+	
+	if isLabel(words[0]):
+		labels[words[0][0:-1]] = currentLine
+	else:
+		opcode = words[0]
+		outputLine += toBinary(int(OP_CODES[opcode]), 4)
+		# am1 används inte, så sätt till 00
+		outputLine += "00"
+		if opcode == "NOP" or opcode == "HALT":
+			outputLine += toBinary(0, 26)
+		elif opcode == "SHIFT":
+			outputLine += toBinary(int(words[1]), 8)
+			outputLine += getMode(words[2])
+			outputLine += toBinary(0, 16)
+		elif opcode == "SETF":
+			outputLine += toBinary(0, 8)
+			outputLine += getMode(words[1])
+			outputLine += toBinary(0, 16)
+		elif opcode == "COL":
+			outputLine += toBinary(int(words[1]), 8)
+			outputLine += toBinary(0, 2)
+			outputLine += COL[words[2]]
+			outputLine += toBinary(0, 14)
+		elif opcode == "BRF":
+			term1 = words[1]
+			
+			if term1 in labels:
+				outputLine += toBinary(labels[term1], 8)
+			else:
+				outputLine += toBinary(int(term1), 8)
+			
+			outputLine += getMode(term1)
+			outputLine += toBinary(0, 16)
+		elif opcode == "BRA":
+			term1 = words[1]
+			
+			if term1 in labels:
+				outputLine += toBinary(int(labels[term1]), 8)
+			else:
+				outputLine += toBinary(int(term1), 8)
+			
+			outputLine += toBinary(0, 18)
+		elif opcode == "CMP":
+			outputLine += toBinary(int(words[1]), 8)
+			outputLine += getMode(words[2])
+			outputLine += toBinary(int(words[3]), 8)
+			outputLine += toBinary(0, 8)
+		else:
+			outputLine += toBinary(int(words[1]), 8)
+			outputLine += getMode(words[2])
+			outputLine += toBinary(int(words[3]), 8)
+			outputLine += toBinary(int(words[4]), 8)
+
+	return outputLine
+
+def isHex(word):
+	return word.startswith("$")
+	
+def isLabel(word):
+	return word.endswith(':')
+
+def toBinary(number, n):
+	return format(number, 'b').zfill(n)
+
+def getMode(word):
+	if word in MODES:
+		return MODES[word]
+	else:
+		return "00"
+	
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Please specify an input file")
+		sys.exit(-1)
+	
+	assemble(sys.argv[1])
